892be86a0feabcd90a0adc32c6a336e8d666af94
Thorn Walli <thorn.walli@grabarzundpartner.intra>
2016-05-31 22:19:47 +0200

    styling and clean

--- src/js/partials/elements/video/Youtube.js
+++ src/js/partials/elements/video/Youtube.js
@@ -10,7 +10,8 @@ module.exports = PositionObserver.extend({
     player: null,
     played: false,
     ready: false,
-        modelConstructor: PositionObserver.prototype.modelConstructor.extend({
+    firstActive: true,
+    modelConstructor: PositionObserver.prototype.modelConstructor.extend({
         session: {
             id: {
                 type: 'string',
@@ -24,44 +25,53 @@ module.exports = PositionObserver.extend({
                 type: 'boolean',
                 default: false
             }
-
-
         }
     }),
 
     initialize: function() {
         PositionObserver.prototype.initialize.apply(this, arguments);
-        var scope = this;
-        youtube.register(function(api) {
-            scope.api = api;
-            scope.setup();
-        });
-
+        this.iframe = this.el.querySelector('iframe');
     },
 
-    setup: function() {
-        console.log('setupPlayer', this.el.getAttribute('id'), this.api);
+    setup: function(callback) {
+        var id = this.iframe.getAttribute('id');
+        console.log('setupPlayer', id, this.api);
         var url = UrlParse(this.model.src, true);
         url.query.enablejsapi = 1;
         console.log(url.toString());
-        this.el.setAttribute('src', url.toString());
-        this.player = new this.api.Player(this.el.getAttribute('id'), {
+        this.iframe.setAttribute('src', url.toString());
+        this.player = new this.api.Player(id, {
             events: {
-                'onReady': this.onReady.bind(this),
+                'onReady': function() {
+                    this.onReady.bind(this)();
+                    callback.bind(this)();
+                }.bind(this),
                 'onStateChange': this.onStateChange.bind(this)
             }
         });
     },
 
     onReady: function() {
-        console.log('ready', this.el.getAttribute('id'), arguments);
         this.ready = true;
         viewport.update();
+        this.el.classList.add('js-ready');
     },
     onStateChange: function() {},
 
     onActive: function() {
-        if (!this.played && this.ready && this.model.inviewAutoplay) {
+        var scope = this;
+        if (this.firstActive) {
+            this.firstActive = false;
+            youtube.register(function(api) {
+                scope.api = api;
+                scope.setup(function() {
+                    if (!this.played && this.ready && this.model.inviewAutoplay) {
+                        this.player.playVideo();
+                        this.played = true;
+                    }
+                });
+            });
+        } else if (!this.played && this.ready && this.model.inviewAutoplay) {
             this.player.playVideo();
             this.played = true;
         }
